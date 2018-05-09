42ce53ad5c3f73bdb29c05a23ab54923e512c663
GCC\TONEYDR1 <toneydr1@gmail.com>
2018-05-08 22:22:19 -0400

    Answer

--- grove.py
+++ grove.py
@@ -1,3 +1,25 @@
+"""
+Is your Grove interpreter using a static or dynamic type system? 
+Briefly explain what aspects of the interpreter make it so.
+
+Our interpriter is using a static type system. 
+We know this is the case because we don't require users to 
+declare a type when they make a new variable. We also have to check 
+on our end to make sure the types match up when doing addition since 
+the user doesn't have to declare what types they are adding. 
+
+Users can also do the following 
+set x = "Hello"
+
+Grove>> set x = "Hello"
+Grove>> x
+Hello
+Grove>> set x = 420
+Grove>> x
+420
+
+"""
+
 from grove_parse import *
 
 if __name__ == "__main__": 
